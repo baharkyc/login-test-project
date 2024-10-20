f933dae4b0eed8b44b7ee5ba7313a988461899c3
baharkyc <baharokuyucu@gmail.com>
2024-10-20 22:15:42 +0300

    Added missing dependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login.jsx';
+
 
 function App() {
   
 
   return (
     <>
-      <p>
-        Main Page
-      </p>
+    <Switch>
+      <Route exact path={'/'}>
+        <Login/>
+      </Route>
+      <Route  path={'/login'}>
+        <Login/>
+      </Route>
+      <Route path={'/success'}>
+
+      </Route>
+    </Switch>
+      
     </>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  FormFeedback,
+} from 'reactstrap';
+import { useHistory } from 'react-router-dom';
+
+import axios from 'axios';
+
+const initialForm = {
+  email: '',
+  password: '',
+  terms: false,
+};
+
+const errorMessages = {
+  email: 'Please enter a valid email address',
+  password: 'Password must be at least 4 characters long',
+  terms: 'Please agree the terms',
+};
+
+export default function Login() {
+  const [form, setForm] = useState(initialForm);
+  const [isValid, setIsValid] = useState(false);
+  const [errors, setErrors] = useState({
+    email: false,
+    password: false,
+    terms: false,
+  });
+
+  const history = useHistory();
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const handleChange = (event) => {
+    let { name, value, type } = event.target;
+    value = type === 'checkbox' ? event.target.checked : value;
+    setForm({ ...form, [name]: value });
+
+    console.log(validateEmail('baharokuyucu'));
+
+    if (name === 'email') {
+      setErrors({ ...errors, [name]: !validateEmail(value) });
+    } else if (name === 'password') {
+      setErrors({ ...errors, [name]: value.length < 4 });
+    } else if (name === 'terms') {
+      setErrors({ ...errors, [name]: !event.target.checked });
+    }
+  };
+
+  useEffect(() => {
+    if (form.terms && form.password.length >= 4 && validateEmail(form.email)) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [form]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isValid) {
+      axios
+        .get('https://6540a96145bedb25bfc247b4.mockapi.io/api/login')
+        .then((res) => {
+          const user = res.data.find(
+            (item) => item.password == form.password && item.email == form.email
+          );
+          if (user) {
+            setForm(initialForm);
+            history.push('/main');
+          } else {
+            history.push('/error');
+          }
+        });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input
+          id="exampleEmail"
+          name="email"
+          placeholder="Enter your email"
+          type="email"
+          onChange={handleChange}
+          value={form.email}
+          invalid={errors.email}
+        />
+        {errors.email && <FormFeedback>{errorMessages.email}</FormFeedback>}
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          id="examplePassword"
+          name="password"
+          placeholder="Enter your password "
+          type="password"
+          onChange={handleChange}
+          value={form.password}
+          invalid={errors.password}
+        />
+        {errors.password && (
+          <FormFeedback>{errorMessages.password}</FormFeedback>
+        )}
+      </FormGroup>
+      <FormGroup check>
+        <Input
+          id="terms"
+          name="terms"
+          checked={form.terms}
+          type="checkbox"
+          onChange={handleChange}
+          invalid={errors.terms}
+        />{' '}
+        <Label htmlFor="terms" check>
+          I agree to terms of service and privacy policy
+        </Label>
+        {errors.terms && <FormFeedback>{errorMessages.terms}</FormFeedback>}
+      </FormGroup>
+      <FormGroup className="text-center p-4">
+        <Button disabled={!isValid} color="primary">
+          Sign In
+        </Button>
+      </FormGroup>
+    </Form>
+  );
+}
