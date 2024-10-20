9ded18b1dfecfe3e3a8ac49b50583a18aae84a3f
baharkyc <baharokuyucu@gmail.com>
2024-10-20 21:40:54 +0300

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+
+  return (
+    <>
+      <p>
+        Main Page
+      </p>
+    </>
+  )
+}
+
+export default App
